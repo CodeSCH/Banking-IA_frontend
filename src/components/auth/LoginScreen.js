585e1b91a65f523c1f9a214183b71b22f0cf0734
Yactayo <syactayocrisostomo@gmail.com>
2022-04-27 09:15:15 -0500

    [Update] fix

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-import React from 'react';
-
-=======
 import '../../styles/components/auth/login.css';
 import { Link } from 'react-router-dom';
->>>>>>> 3314d8ed7332852be22dd8745b2f784fb1c61da2
+
 export const LoginScreen = () => {
   /*eslint-disable*/
   return (
@@ -23,7 +19,7 @@ export const LoginScreen = () => {
               <br></br>
               <input type="submit" value="Login" />
               <p className="signup">
-              ¿Aún no tienes cuenta?<Link to="/auth/register">Registrarse</Link>
+                ¿Aún no tienes cuenta?<Link to="/auth/register">Registrarse</Link>
               </p>
             </form>
           </div>
