9ab392dfd9908b203a66db9ae2edd6e7343f1f5b
APEREZB26 <aperez26@autonoma.edu.pe>
2022-05-01 14:02:08 -0500

    [ADD] MAINSCREEN UI

--- src/components/banking/MainScreen.js
+++ src/components/banking/MainScreen.js
@@ -1,5 +1,23 @@
 import { NavBar } from '../ui/NavBar';
+import { DepositCard } from '../ui/DepositCard';
+import { TransferCard } from '../ui/TransferCard';
+import { HistorialCard } from '../ui/HistorialCard';
+import { UserInfo } from '../ui/UserInfo';
 
 export const MainScreen = () => {
-  return <NavBar />;
+  return (
+    <div>
+      <NavBar />
+      <div className="Wrapper">
+        <div className="UserInfo__cont">
+          <UserInfo />
+          <div>
+            <DepositCard />
+            <TransferCard />
+          </div>
+        </div>
+        <HistorialCard />
+      </div>
+    </div>
+  );
 };
---
+++ src/components/ui/HistorialCard.js
@@ -0,0 +1,12 @@
+export const HistorialCard = () => {
+  return (
+    <div className="Historial__card__container">
+      <div className="card__header">
+        <h3>HistorialCard</h3>
+      </div>
+      <div className="card__body">
+        <h6>Historial de Transacciones</h6>
+      </div>
+    </div>
+  );
+};
